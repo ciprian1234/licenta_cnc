6d98ee258fa86edbc516fe257b24a1e298a5eaf3
AndreiCiprian <andrei.stefanel.ciprian@gmail.com>
2019-01-29 19:43:08 +0200

    Updated docs, started diagrams

--- test/main.py
+++
@@ -1,9 +0,0 @@
-import time, serial
-
-# ser = serial.Serial('COM5', 9600)
-#
-# while True:
-#     cmd = input(">>>")
-#     ser.write(ord(cmd))
-#     response = ser.read()
-#     print("Recieved: [%s]"%response);
